4acf43fbb40bdd58993870e1275a7fe98aad3f65
Teja Puttapaka <sriteja.puttapaka@gmail.com>
2024-01-15 09:58:22 -0600

    Update Mitigation.py

--- Mitigation/Mitigation.py
+++ Mitigation/Mitigation.py
@@ -6,7 +6,7 @@ def deleteProcesses(filePath):
    print("\nFile Path:", filePath)
    com = "for proc in $(lsof -R " + filePath + "|grep defender|awk '{print $1}'); do echo 'Killing process : '$proc; pkill -9 $proc; done"
    print(com)
-   os.system(com)
+   os.system(com) 
    print("\nKilled")
   
 #deleteProcesses(paths)
